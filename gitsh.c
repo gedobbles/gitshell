6d28d8d718328d93bbb28fce02deac052bcfd6e3
gedobbles <gedobbles@web.de>
2020-06-20 15:42:31 +0200

    Added git command and external command completion on first level

--- gitsh.c
+++ gitsh.c
@@ -13,6 +13,9 @@
 #define HISTORY_FILE "/.gitsh"
 #define HISTORY_FILE_CHRS 8   //make sure this is correct! (len+1 for \0)
 
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+
 //#define DBG 1   //comment out to switch DBG-output off
 
 /*
@@ -21,7 +24,7 @@
      - allow the colon for external cmds to be not seperated          ✓
      - add cd                                                         ✓
      - add better completion:
-       * git command completion
+       * git command completion                                       ✓
           ~ branch: branches
           ~ checkout: branches
           ~ config: (some) variables
@@ -38,7 +41,7 @@
           ~ reset: branches
           ~ show: branches
 
-       * after ':' program name completion on first level
+       * after ':' program name completion on first level             ✓
 
 */
 
@@ -52,10 +55,34 @@ int    arr_elements(char**);
 
 char** split_line(char*);
 
+//readline completion
+void initialize_readline();
+int getExtCmds();
+
+char* command_generator(const char *, int);
+char* ext_command_generator(const char *, int);
+char** gitsh_completion(const char *, int, int);
+char* dupstr(char*);
+
 // constants
 char* git = "git";
 
 char* histfile;   //name of the history file
+
+char* gitsh_cmds[] = {"add", "checkout", "difftool", "imap-send", "notes",
+                      "reset", "submodule", "am", "cherry", "fetch", "init",
+                      "pull", "revert", "subtree", "annotate", "cherry-pick",
+                      "filter-branch","instaweb", "push", "rm", "tag", "apply",
+                      "clean", "format-patch", "interpret-trailers", "rebase",
+                      "shortlog", "verify-commit", "archive", "clone", "fsck",
+                      "log", "reflog", "show", "whatchanged", "bisect",
+                      "commit", "gc", "merge", "remote", "show-branch",
+                      "worktree", "blame", "config", "get-tar-commit-id",
+                      "mergetool", "repack", "stage", "branch", "describe",
+                      "grep", "mv", "replace", "stash", "bundle", "diff",
+                      "help", "name-rev", "request-pull", "status", "exit"    };
+
+char** ext_cmds;
 
 int main()
 {
@@ -69,6 +96,7 @@ int main()
            "Use a preceding colon for external commands.\n\n");
 
     init(0);   //Read history file if existing, else create
+    initialize_readline();
 
     while (running) {
       input = readline("(git) > ");
@@ -220,7 +248,8 @@ int arr_elements(char** args)
 }
 
 
-void init(int reinit) {
+void init(int reinit)
+{
   char* dir = getcwd(NULL, 0);
   int dirlen = 0;
   while (dir[dirlen] != 0) {
@@ -249,4 +278,223 @@ void init(int reinit) {
     start_proc(a);
     free(a);
   }
+}
+
+// readline completion
+
+
+
+/* Tell the GNU Readline library how to complete.  We want to try to
+   complete on command names if this is the first word in the line, or
+   on filenames if not. */
+void initialize_readline()
+{
+  getExtCmds();
+  rl_attempted_completion_function = gitsh_completion;
+}
+
+/* Attempt to complete on the contents of TEXT.  START and END
+   bound the region of rl_line_buffer that contains the word to
+   complete.  TEXT is the word to complete.  We can use the entire
+   contents of rl_line_buffer in case we want to do some simple
+   parsing.  Returnthe array of matches, or NULL if there aren't any. */
+char** gitsh_completion (const char* text, int start, int end)
+{
+  char **matches;
+  matches = (char **)NULL;
+
+  /* If this word is at the start of the line, then it is a command
+     to complete.  Otherwise it is the name of a file in the current
+     directory. */
+  if (start == 0) {
+    if (*text == ':') {
+      matches = rl_completion_matches(text, ext_command_generator);
+    }else{
+      matches = rl_completion_matches(text, command_generator);
+    }
+  }
+
+  //filename completion if we return NULL
+  return (matches);
+}
+
+// generator for gitsh commands
+char* command_generator(const char* text, int state)
+{
+  static int i, len;
+  char *cmd;
+
+  /* If this is a new word to complete, initialize now.  This
+     includes saving the length of TEXT for efficiency, and
+     initializing the index variable to 0. */
+  if (!state)
+    {
+      i = 0;
+      len = strlen (text);
+    }
+
+  /* Return the next name which partially matches from the
+     command list. */
+  while (cmd = gitsh_cmds[i])
+    {
+      i++;
+      if (strncmp (cmd, text, len) == 0) {
+        return (dupstr(cmd));
+      }
+    }
+
+  /* If no names matched, then return NULL. */
+  return ((char *)NULL);
+}
+
+//generator for external commands
+char* ext_command_generator(const char* text, int state)
+{
+  static int i, len;
+  char *cmd;
+
+  /* If this is a new word to complete, initialize now.  This
+     includes saving the length of TEXT for efficiency, and
+     initializing the index variable to 0. */
+  if (!state)
+    {
+      i = 0;
+      len = strlen (text);
+    }
+
+  /* Return the next name which partially matches from the
+     command list. */
+  while (cmd = ext_cmds[i])
+    {
+      i++;
+      if (strncmp (cmd, text, len) == 0) {
+        return (dupstr(cmd));
+      }
+    }
+
+  /* If no names matched, then return NULL. */
+  return ((char *)NULL);
+}
+
+char* dupstr(char* s)
+{
+  char *r;
+
+  r = (char*) malloc (strlen (s) + 1);
+  strcpy (r, s);
+  return (r);
+}
+
+int getExtCmds()
+{
+  int aStdinPipe[2];
+  int aStdoutPipe[2];
+  int pidChild, childResult;
+  int toChild, fromChild;
+
+  if (pipe(aStdinPipe) < 0) {
+    perror("allocating pipe for child input redirect");
+    return -1;
+  }
+  if (pipe(aStdoutPipe) < 0) {
+    close(aStdinPipe[PIPE_READ]);
+    close(aStdinPipe[PIPE_WRITE]);
+    perror("allocating pipe for child output redirect");
+    return -1;
+  }
+
+  pidChild = fork();
+  if (0 == pidChild) {
+    // child continues here
+
+    // redirect stdin
+    if (dup2(aStdinPipe[PIPE_READ], STDIN_FILENO) == -1) {
+      exit(-1);
+    }
+
+    // redirect stdout
+    if (dup2(aStdoutPipe[PIPE_WRITE], STDOUT_FILENO) == -1) {
+      exit(-1);
+    }
+
+    // redirect stderr
+    if (dup2(aStdoutPipe[PIPE_WRITE], STDERR_FILENO) == -1) {
+      exit(-1);
+    }
+
+    // all these are for use by parent only
+    close(aStdinPipe[PIPE_READ]);
+    close(aStdinPipe[PIPE_WRITE]);
+    close(aStdoutPipe[PIPE_READ]);
+    close(aStdoutPipe[PIPE_WRITE]);
+
+    // run "bash -c""
+    char* bash    = "bash";
+    char* _c      = "-c";
+    char* compgen = "compgen -c";
+    char** command = (char**)malloc(sizeof(char*)*5);
+    command[0] = bash;
+    command[1] = _c;
+    command[2] = compgen;
+    command[3] = _c;
+    command[4] = NULL;
+    childResult = execvp(command[0], command);
+
+    // if we get here at all, an error occurred, but we are in the child
+    // process, so just exit
+    exit(childResult);
+  } else if (pidChild > 0) {
+    // parent continues here
+
+    // close unused file descriptors, these are for child only
+    close(aStdinPipe[PIPE_READ]);
+    close(aStdoutPipe[PIPE_WRITE]);
+
+    // keep as easier names
+    fromChild = aStdoutPipe[PIPE_READ];
+
+
+    char c;
+    char temp[50];
+    int temp_i = 0;
+    int cmd_i = 0;
+    char* cmd_temp[5000];
+
+    while(read(fromChild, &c, 1)!=0)
+    {
+      if (temp_i == 0) {
+        *temp = ':';
+        temp_i ++;
+      }
+      if (c == '\n') {
+        temp[temp_i] = 0;
+        cmd_temp[cmd_i] = dupstr(temp);
+        cmd_i++;
+        temp_i = 0;
+      }else{
+        temp[temp_i] = c;
+        temp_i++;
+      }
+      //write(STDOUT_FILENO, &c, 1);    //DBG
+    }
+    //last command
+    temp[temp_i] = 0;
+    cmd_temp[cmd_i] = dupstr(temp);
+
+    ext_cmds = (char**)malloc(sizeof(char*)*(cmd_i+1));
+    for (int i = 0; i < (cmd_i+1); i++) {
+      ext_cmds[i] = cmd_temp[i];
+      printf("%s\n", ext_cmds[i]);
+    }
+
+
+  } else {
+    // failed to create child
+    close(aStdinPipe[PIPE_READ]);
+    close(aStdinPipe[PIPE_WRITE]);
+    close(aStdoutPipe[PIPE_READ]);
+    close(aStdoutPipe[PIPE_WRITE]);
+    return 1;
+  }
+  return 0;
 }
